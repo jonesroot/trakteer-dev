fdcd384b6405c59734ce9c986040b50ab637e95b
Lucifer <38715155+jonesroot@users.noreply.github.com>
2025-01-08 15:56:45 +0700

    update trakteer_dev/__init__.py

--- trakteer_dev/__init__.py
+++ trakteer_dev/__init__.py
@@ -1,7 +1,7 @@
-from ._client import Client
+from ._client import Watcher
 from ._data_types import TrakteerDonationData
 from ._exception import (TrakteerMethodUnoverridable, TrakteerMissingStreamKey,
                          TrakteerMissingUserHash, TrakteerWebsocketError)
 from ._logging import logger
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
