c0e623b4758e689f5b69458fed476a0a36f5fdc2
Lucifer <38715155+jonesroot@users.noreply.github.com>
2025-01-08 12:32:24 +0700

    update trakteer_dev/_logging.py

---
+++ trakteer_dev/_logging.py
@@ -0,0 +1,22 @@
+import logging
+import os
+import sys
+from datetime import datetime
+
+from pytz import timezone
+from rich.console import Console
+from rich.logging import RichHandler
+
+console = Console()
+_RHandler = RichHandler(console=console, rich_tracebacks=True)
+
+
+
+logging.getLogger("trakteer").setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    "[%(asctime)s] [%(process)d] [%(levelname)s] %(message)s", "%Y-%m-%d %H:%M"
+)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(_RHandler)
+_RHandler.setFormatter(formatter)
