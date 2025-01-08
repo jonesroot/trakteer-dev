22ef5a554b4d7988bc6352a2edc1f79dca7161f1
Lucifer <38715155+jonesroot@users.noreply.github.com>
2025-01-08 11:28:30 +0700

    update src/trakteer_dev/_exception.py

---
+++ src/trakteer_dev/_exception.py
@@ -0,0 +1,16 @@
+
+
+class TrakteerMissingUserHash(Exception):
+    pass
+
+
+class TrakteerMissingStreamKey(Exception):
+    pass
+
+
+class TrakteerWebsocketError(Exception):
+    pass
+
+
+class TrakteerMethodUnoverridable(Exception):
+    pass
